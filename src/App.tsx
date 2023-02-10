cba827e4457d595d50beadbd2e5ddfec439151e9
Ahmed K.A <ahmadkabdullah@pm.me>
2023-02-10 19:42:10 +0300

    Add global context for rtl and darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { createEmotionCache, MantineProvider } from "@mantine/core";
 import { useLocalStorage } from "@mantine/hooks";
+import { createContext, useContext } from "react";
 import rtlPlugin from "stylis-plugin-rtl";
+import { Hello } from "./components/Hello";
 
 // right to left caching for emotion
 const rtlCache = createEmotionCache({
 	key: "mantine-rtl",
 	stylisPlugins: [rtlPlugin],
 });
+
+// app context to provide rtl and darkmode state
+export const AppContext = createContext({
+	toggleRtl: () => {},
+	toggleDarkMode: () => {},
+});
+
+// function to abstract the usecontext hook
+export const useAppContext = () => useContext(AppContext);
 
 function App() {
 	// set both rtl and darkmode in localStorage
@@ -26,9 +37,16 @@ function App() {
 			}}
 			emotionCache={rtl ? rtlCache : undefined}
 		>
-			<div dir={rtl ? "rtl" : "ltr"}>
-				<h1>Hello</h1>
-			</div>
+			<AppContext.Provider
+				value={{
+					toggleRtl: () => setRtl((old) => !old),
+					toggleDarkMode: () => setDarkMode((old) => !old),
+				}}
+			>
+				<div dir={rtl ? "rtl" : "ltr"}>
+					<Hello></Hello>
+				</div>
+			</AppContext.Provider>
 		</MantineProvider>
 	);
 }
---
+++ src/components/Hello.tsx
@@ -0,0 +1,9 @@
+import { useAppContext } from "../App";
+
+export function Hello() {
+	return (
+		<div>
+			<h1>Hello</h1>
+		</div>
+	);
+}
