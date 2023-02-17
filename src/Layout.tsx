568c0be492769eee74e94d454739fb5a35d1f85b
Ahmed K.A <ahmadkabdullah@pm.me>
2023-02-17 12:46:41 +0300

    Add heroicons and set structure for navbar

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,14 @@
 import { ReactNode } from "react";
-import { AppShell, Header, Navbar } from "@mantine/core";
+import {
+	ActionIcon,
+	AppShell,
+	Center,
+	Flex,
+	Header,
+	Navbar,
+	Stack,
+} from "@mantine/core";
+import { BookOpenIcon, HomeIcon } from "@heroicons/react/24/solid";
 
 // the overall layout of the app
 export function Layout(props: { children: ReactNode; rtl: boolean }) {
@@ -13,8 +22,34 @@ export function Layout(props: { children: ReactNode; rtl: boolean }) {
 					</Header>
 				}
 				navbar={
-					<Navbar width={{ base: 100, md: 300 }} p="sm">
-						Navbar
+					<Navbar width={{ base: 50, sm: 75 }} p="md">
+						<Stack
+							align={"center"}
+							justify={"space-between"}
+							spacing={16}
+							h={"100%"}
+						>
+							{/* Upper */}
+							<Stack align={"center"} spacing={16}>
+								<ActionIcon p={2} size={"lg"} mb={"lg"}>
+									<BookOpenIcon></BookOpenIcon>
+								</ActionIcon>
+								{/* Navigation Items */}
+								<ActionIcon p={2} size={"lg"}>
+									<HomeIcon></HomeIcon>
+								</ActionIcon>
+								<ActionIcon p={2} size={"lg"}>
+									<HomeIcon></HomeIcon>
+								</ActionIcon>
+								{/* Navigation Items End */}
+							</Stack>
+							{/* Upper End */}
+							{/* Lower */}
+							<ActionIcon size={"lg"}>
+								<BookOpenIcon></BookOpenIcon>
+							</ActionIcon>
+							{/* Lower End */}
+						</Stack>
 					</Navbar>
 				}
 			>
