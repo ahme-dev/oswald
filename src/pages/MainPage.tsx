b6684f2b3b63502ac71e0cc5125ccf971274f56e
Ahmed K.A <ahmadkabdullah@pm.me>
2023-02-23 19:57:05 +0300

    Improve responsivity of MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,6 +6,7 @@ import {
 	Text,
 	Button,
 	Group,
+	Grid,
 } from "@mantine/core";
 import { ProductList } from "../components/ProductList";
 import { useDBFiltered } from "../utils/useDB";
@@ -16,31 +17,35 @@ export function MainPage() {
 	});
 
 	return (
-		<Stack>
+		<Stack h={"100%"}>
 			<Title size={"h2"} weight="bold">
 				Sell
 			</Title>
-			<SimpleGrid cols={2}>
-				<ProductList
-					data={filterQuery.data}
-					loading={filterQuery.loading}
-				></ProductList>
-				<Card>
-					<Stack spacing={"md"}>
-						<Group position="apart">
-							<Title size={"h3"}>Customer</Title>
-						</Group>
-						<Stack spacing={"sm"}>
-							<Text>QTY 1 - item1</Text>
-							<Text>QTY 2 - item1</Text>
+			<Grid h={"100%"}>
+				<Grid.Col span={12} md={7}>
+					<ProductList
+						data={filterQuery.data}
+						loading={filterQuery.loading}
+					></ProductList>
+				</Grid.Col>
+				<Grid.Col span={12} md={5}>
+					<Card>
+						<Stack spacing={"md"} justify={"space-between"}>
+							<Group position="apart">
+								<Title size={"h3"}>Customer</Title>
+							</Group>
+							<Stack spacing={"sm"}>
+								<Text>QTY 1 - item1</Text>
+								<Text>QTY 2 - item1</Text>
+							</Stack>
+							<Group position="apart">
+								<Title size={"h3"}>Total</Title>
+								<Button>Checkout</Button>
+							</Group>
 						</Stack>
-						<Group position="apart">
-							<Title size={"h3"}>Total</Title>
-							<Button>Checkout</Button>
-						</Group>
-					</Stack>
-				</Card>
-			</SimpleGrid>
+					</Card>
+				</Grid.Col>
+			</Grid>
 		</Stack>
 	);
 }
