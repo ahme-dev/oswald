b94bf2b76865c89b453a0c809621b61261b81517
Ahmed K.A <ahmadkabdullah@pm.me>
2023-03-02 23:12:15 +0300

    Move Checkout into separate component

---
+++ src/components/Checkout.tsx
@@ -0,0 +1,86 @@
+import {
+	Card,
+	Stack,
+	Title,
+	Text,
+	Button,
+	Group,
+	Divider,
+	NumberInput,
+} from "@mantine/core";
+import { useTranslation } from "react-i18next";
+import { CheckoutState } from "../stores/checkout";
+
+export function Checkout(props: {
+	state: CheckoutState;
+	clear: () => void;
+	apply: () => void;
+	changeQuantity: (id: string, qty: number) => void;
+}) {
+	const { t } = useTranslation();
+
+	return (
+		<Stack spacing={"md"} justify={"space-between"}>
+			<Group position="apart">
+				<Title size={"h3"}>{t("Customer")}</Title>
+			</Group>
+			{/* Checkout items from store */}
+			<Stack spacing={"sm"}>
+				{props.state.items.length === 0 ? (
+					<Text>{t("No items in checkout")}</Text>
+				) : (
+					props.state.items.map((chItem) => (
+						<Card py="xs" key={chItem.id}>
+							<Group position="apart">
+								<Group spacing={"sm"}>
+									<Text weight={"bold"}>{chItem.price}</Text>
+									<Divider size="md" orientation="vertical"></Divider>
+									<Text weight={"bold"}>{chItem.name}</Text>
+								</Group>
+								<Group spacing={"sm"}>
+									<NumberInput
+										min={1}
+										sx={{ width: "4rem" }}
+										value={chItem.qty}
+										onChange={(evt: any) => {
+											props.changeQuantity(chItem.id, evt);
+										}}
+									></NumberInput>
+								</Group>
+							</Group>
+						</Card>
+					))
+				)}
+			</Stack>
+			{/* Checkout items from store end */}
+			<Group position="apart" align={"center"}>
+				<Card py={"xs"}>
+					<Group>
+						<Group spacing={"xs"}>
+							<Text weight={"bold"}>{props.state.total}</Text>
+							<Text weight={"bold"}>{t("in total")}</Text>
+						</Group>
+						<Divider size="md" orientation="vertical"></Divider>
+						<Group spacing={"xs"}>
+							<Text weight={"bold"}>
+								{props.state.items.reduce((total, item) => {
+									total += item.qty;
+									return total;
+								}, 0)}
+							</Text>
+							<Text weight={"bold"}>{t("items")}</Text>
+						</Group>
+					</Group>
+				</Card>
+				<Group>
+					<Button variant="light" onClick={() => props.clear()}>
+						{t("Clear")}
+					</Button>
+					<Button variant="light" onClick={() => props.apply()}>
+						{t("Checkout")}
+					</Button>
+				</Group>
+			</Group>
+		</Stack>
+	);
+}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -10,6 +10,7 @@ import {
 	NumberInput,
 } from "@mantine/core";
 import { useTranslation } from "react-i18next";
+import { Checkout } from "../components/Checkout";
 import { ProductList } from "../components/ProductList";
 import { TitleText } from "../components/TitleText";
 import { apply, CheckoutState } from "../stores/checkout";
@@ -27,6 +28,15 @@ export function MainPage() {
 	let checkoutState: CheckoutState = useAppSelector(checkoutSelector);
 	let dispatch = useAppDispatch();
 
+	const qtyFunc = (id: string, qty: number) => {
+		dispatch(
+			checkoutActions.setItemQty({
+				id,
+				qty,
+			}),
+		);
+	};
+
 	return (
 		<Stack h={"100%"}>
 			<TitleText title={t("Sell")} />
@@ -41,81 +51,12 @@ export function MainPage() {
 					></ProductList>
 				</Grid.Col>
 				<Grid.Col span={12} sm={5}>
-					<Stack spacing={"md"} justify={"space-between"}>
-						<Group position="apart">
-							<Title size={"h3"}>{t("Customer")}</Title>
-						</Group>
-						{/* Checkout items from store */}
-						<Stack spacing={"sm"}>
-							{checkoutState.items.length === 0 ? (
-								<Text>{t("No items in checkout")}</Text>
-							) : (
-								checkoutState.items.map((chItem) => (
-									<Card py="xs" key={chItem.id}>
-										<Group position="apart">
-											<Group spacing={"sm"}>
-												<Text weight={"bold"}>{chItem.price}</Text>
-												<Divider size="md" orientation="vertical"></Divider>
-												<Text weight={"bold"}>{chItem.name}</Text>
-											</Group>
-											<Group spacing={"sm"}>
-												<NumberInput
-													min={1}
-													sx={{ width: "4rem" }}
-													value={chItem.qty}
-													onChange={(evt: any) => {
-														dispatch(
-															checkoutActions.setItemQty({
-																id: chItem.id,
-																qty: evt,
-															}),
-														);
-													}}
-												></NumberInput>
-											</Group>
-										</Group>
-									</Card>
-								))
-							)}
-						</Stack>
-						{/* Checkout items from store end */}
-						<Group position="apart" align={"center"}>
-							<Card py={"xs"}>
-								<Group>
-									<Group spacing={"xs"}>
-										<Text weight={"bold"}>{checkoutState.total}</Text>
-										<Text weight={"bold"}>{t("in total")}</Text>
-									</Group>
-									<Divider size="md" orientation="vertical"></Divider>
-									<Group spacing={"xs"}>
-										<Text weight={"bold"}>
-											{checkoutState.items.reduce((total, item) => {
-												total += item.qty;
-												return total;
-											}, 0)}
-										</Text>
-										<Text weight={"bold"}>{t("items")}</Text>
-										{/* <Title size={"h4"}>{checkoutState.total}</Title>
-									<Title size={"h4"}>{t("in total")}</Title> */}
-									</Group>
-								</Group>
-							</Card>
-							<Group>
-								<Button
-									variant="light"
-									onClick={() => dispatch(checkoutActions.clear())}
-								>
-									{t("Clear")}
-								</Button>
-								<Button
-									variant="light"
-									onClick={() => dispatch(apply(checkoutState.items))}
-								>
-									{t("Checkout")}
-								</Button>
-							</Group>
-						</Group>
-					</Stack>
+					<Checkout
+						state={checkoutState}
+						apply={() => dispatch(apply(checkoutState.items))}
+						clear={() => dispatch(checkoutActions.clear())}
+						changeQuantity={qtyFunc}
+					></Checkout>
 				</Grid.Col>
 			</Grid>
 		</Stack>
