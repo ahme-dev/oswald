a323b7d7602dced2f4d8cd04062cdcb327201f8c
Ahmed K.A <ahmadkabdullah@pm.me>
2023-03-02 17:32:53 +0300

    Add data fetching and display to transactions page

--- src/pages/TransactionsPage.tsx
+++ src/pages/TransactionsPage.tsx
@@ -1,11 +1,24 @@
-import { Text } from "@mantine/core";
+import { Card, Flex, Loader, Stack, Text, Title } from "@mantine/core";
+import { useCollection } from "../utils/pb";
 
 export function TransactionsPage() {
+	let query = useCollection("transactions");
+
+	if (query.loading) return <Loader></Loader>;
+	if (!query.data) return <Text>Hi</Text>;
+
 	return (
-		<div>
-			<Text fz={"xl"} weight={"bold"}>
-				transactions
-			</Text>
-		</div>
+		<Stack h={"100%"}>
+			<Flex justify={"space-between"} align="center" gap={"lg"}>
+				<Title size={"h2"} weight="bold">
+					Transactions
+				</Title>
+			</Flex>
+			<Stack>
+				{query.data.items.map((item) => {
+					return <Card key={item.id}>{item.date}</Card>;
+				})}
+			</Stack>
+		</Stack>
 	);
 }
