0e2a9b4d78d6aeec7c80f5a4e5890320c32a5831
Ahmed K.A <ahmadkabdullah@pm.me>
2023-03-05 13:47:16 +0300

    Merge both drawers and form using a mode property

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -35,46 +35,28 @@ export function ProductsPage() {
 
 	// search string and drawer visiblity state
 	let [search, setSearch] = useState("");
-	let [addDrawerVisible, setAddDrawerVisible] = useState(false);
-	let [editDrawerVisible, setEditDrawerVisible] = useState(false);
+	let [drawerVisible, setDrawerVisible] = useState(false);
 
-	const addForm = useForm({
-		initialValues: {
-			name: "",
-			price_current: 1000,
-			quantity_available: 1,
-			about: "",
-			category: {
-				id: "",
-				name: "",
-			},
-		},
-
-		validate: {
-			name: (value) => (value.length > 0 ? null : "Name is required"),
-			price_current: (value) => (value > 0 ? null : "Price is required"),
-			quantity_available: (value) =>
-				value > 0 ? null : "Quantity is required",
-			about: (value) => (value.length > 0 ? null : "About is required"),
-		},
-	});
-
-	const editForm = useForm({
-		initialValues: {
+	const initialForm = {
+		mode: "add",
+		id: "",
+		name: "",
+		price_current: 250,
+		quantity_available: 1,
+		about: "",
+		category: {
 			id: "",
 			name: "",
-			price_current: 0,
-			quantity_available: 0,
-			about: "",
-			category: {
-				id: "",
-				name: "",
-			},
 		},
+	};
+
+	const form = useForm({
+		initialValues: initialForm,
 
 		validate: {
-			name: (value) => (value.length > 0 ? null : "Name is required"),
-			price_current: (value) => (value > 0 ? null : "Price is required"),
+			name: (value) => (value.length > 3 ? null : "Name is required"),
+			price_current: (value) =>
+				value >= 250 ? null : "Price must be 250 or higher",
 			quantity_available: (value) =>
 				value > 0 ? null : "Quantity is required",
 			about: (value) => (value.length > 0 ? null : "About is required"),
@@ -84,37 +66,52 @@ export function ProductsPage() {
 	const tryAddProduct = async () => {
 		// try to validate the form
 		// if got errors, return
-		let feedback = addForm.validate();
+		let feedback = form.validate();
 		if (feedback.hasErrors) return;
 
 		dispatch(
 			createProduct({
-				...addForm.values,
+				name: form.values.name,
+				about: form.values.about,
+				category: {
+					name: form.values.category.name,
+					id: form.values.category.id,
+				},
+				price_current: form.values.price_current,
+				quantity_available: form.values.quantity_available,
 			}),
 		);
 
-		setAddDrawerVisible(false);
+		setDrawerVisible(false);
 	};
 
 	const tryEditProduct = async () => {
 		// try to validate the form
 		// if got errors, return
-		let feedback = editForm.validate();
+		let feedback = form.validate();
 		if (feedback.hasErrors) return;
 
 		dispatch(
 			editProduct({
-				...editForm.values,
+				id: form.values.id,
+				name: form.values.name,
+				about: form.values.about,
+				category: {
+					name: form.values.category.name,
+					id: form.values.category.id,
+				},
+				price_current: form.values.price_current,
+				quantity_available: form.values.quantity_available,
 			}),
 		);
 
-		setEditDrawerVisible(false);
+		setDrawerVisible(false);
 	};
 
 	const tryDeleteProduct = async () => {
-		dispatch(deleteProduct({ id: editForm.values.id }));
+		dispatch(deleteProduct({ id: form.values.id }));
 
-		setEditDrawerVisible(false);
+		setDrawerVisible(false);
 	};
 
 	// render
@@ -133,15 +130,55 @@ export function ProductsPage() {
 				loading={productsState.loading}
 				filterTerms={search}
 				itemClickFunc={(product) => {
-					setEditDrawerVisible(true);
-					editForm.setValues({
+					//  set edit mode with product info on form and oepn drawer
+					form.setValues({
 						...product,
+						mode: "edit",
 					});
+					setDrawerVisible(true);
 				}}
 			></ProductList>
 
+			{/* Drawer for adding / editing a product */}
+			<Drawer
+				opened={drawerVisible}
+				position={settingsState.rightToLeft ? "right" : "left"}
+				onClose={() => setDrawerVisible(false)}
+				title={
+					form.values.mode === "edit" ? t("Edit a product") : t("Add a product")
+				}
+				overlayOpacity={0.7}
+				padding="xl"
+				size="xl"
+			>
+				<Stack>
+					<FormElements formObject={form} />
+
+					{form.values.mode === "edit" ? (
+						<SimpleGrid cols={2} mt={16}>
+							<Button onClick={() => tryEditProduct()}>{t("Change")}</Button>
+							<Button variant="light" onClick={() => tryDeleteProduct()}>
+								{t("Delete")}
+							</Button>
+						</SimpleGrid>
+					) : (
+						<Button color={settingsState.color} onClick={tryAddProduct} mt={16}>
+							{t("Add")}
+						</Button>
+					)}
+				</Stack>
+			</Drawer>
+
+			{/* Add product button */}
 			<Affix
-				onClick={() => setAddDrawerVisible(true)}
+				onClick={() => {
+					// set add mode with empty form, and open drawer
+					form.setValues({
+						...initialForm,
+						mode: "add",
+					});
+					setDrawerVisible(true);
+				}}
 				position={{ bottom: 20, right: 20 }}
 			>
 				<ActionIcon
@@ -154,141 +191,70 @@ export function ProductsPage() {
 					<PlusIcon></PlusIcon>
 				</ActionIcon>
 			</Affix>
-
-			{/* Drawer for editing a product */}
-			<Drawer
-				opened={editDrawerVisible}
-				position={settingsState.rightToLeft ? "right" : "left"}
-				onClose={() => setEditDrawerVisible(false)}
-				title={t("Edit a product")}
-				overlayOpacity={0.7}
-				padding="xl"
-				size="xl"
-			>
-				<Stack>
-					<TextInput
-						withAsterisk
-						label={t("Name")}
-						placeholder={t("Product name") || "Product name"}
-						{...editForm.getInputProps("name")}
-					/>
-					<Select
-						label={t("Category")}
-						clearable
-						{...editForm.getInputProps("category.id")}
-						onChange={(e) => {
-							editForm.setFieldValue("category.id", e);
-						}}
-						data={productsState.categories.map((e) => {
-							return {
-								value: e.id,
-								label: t(e.name) || e.name,
-							};
-						})}
-						searchable
-						creatable
-						getCreateLabel={(query) => t("Create ") + query}
-						onCreate={(query) => {
-							const item = { name: query };
-							dispatch(createCategory(item));
-							return item.name;
-						}}
-					/>
-
-					<Textarea
-						withAsterisk
-						label={t("About")}
-						placeholder={t("About the product") || "About the product"}
-						{...editForm.getInputProps("about")}
-					/>
-
-					<Group position="apart">
-						<Flex direction={"row"} gap="lg">
-							<NumberInput
-								withAsterisk
-								label={t("Price")}
-								step={250}
-								placeholder={t("Product price") || "Product price"}
-								{...editForm.getInputProps("price_current")}
-							/>
-							<NumberInput
-								withAsterisk
-								label={t("Quantity")}
-								placeholder={t("Quantity available") || "Quantity available"}
-								{...editForm.getInputProps("quantity_available")}
-							/>
-						</Flex>
-					</Group>
-
-					<SimpleGrid cols={2} mt={16}>
-						<Button onClick={() => tryEditProduct()}>{t("Change")}</Button>
-						<Button variant="light" onClick={() => tryDeleteProduct()}>
-							{t("Delete")}
-						</Button>
-					</SimpleGrid>
-				</Stack>
-			</Drawer>
-
-			{/* Drawer for adding a product */}
-			<Drawer
-				opened={addDrawerVisible}
-				position={settingsState.rightToLeft ? "right" : "left"}
-				onClose={() => setAddDrawerVisible(false)}
-				title={t("Add a product")}
-				overlayOpacity={0.7}
-				padding="xl"
-				size="xl"
-			>
-				<Stack>
-					<TextInput
-						withAsterisk
-						label={t("Name")}
-						placeholder={t("Product name") || "Product name"}
-						{...addForm.getInputProps("name")}
-					/>
-					<Select
-						label={t("Category")}
-						clearable
-						onChange={(e) => {
-							addForm.setFieldValue("category.id", e);
-						}}
-						data={productsState.categories.map((e) => {
-							return {
-								value: e.id,
-								label: t(e.name) || e.name,
-							};
-						})}
-					/>
-					<Textarea
-						withAsterisk
-						label={t("About")}
-						placeholder={t("About the product") || "About the product"}
-						{...addForm.getInputProps("about")}
-					/>
-
-					<Group position="apart">
-						<Flex direction={"row"} gap="lg">
-							<NumberInput
-								withAsterisk
-								label={t("Price")}
-								step={250}
-								placeholder={t("Product price") || "Product price"}
-								{...addForm.getInputProps("price")}
-							/>
-							<NumberInput
-								withAsterisk
-								label={t("Quantity")}
-								placeholder={t("Quantity available") || "Quantity available"}
-								{...addForm.getInputProps("quantity")}
-							/>
-						</Flex>
-					</Group>
-
-					<Button color={settingsState.color} onClick={tryAddProduct} mt={16}>
-						{t("Add")}
-					</Button>
-				</Stack>
-			</Drawer>
+			{/* Add product button end */}
 		</Stack>
+	);
+}
+
+function FormElements(props: { formObject: any }) {
+	let productsState = useAppSelector((state) => state.products);
+	let dispatch = useAppDispatch();
+
+	return (
+		<>
+			<TextInput
+				withAsterisk
+				label={t("Name")}
+				placeholder={t("Product name") || "Product name"}
+				{...props.formObject.getInputProps("name")}
+			/>
+			<Select
+				label={t("Category")}
+				clearable
+				{...props.formObject.getInputProps("category.id")}
+				onChange={(e) => {
+					props.formObject.setFieldValue("category.id", e);
+				}}
+				data={productsState.categories.map((e) => {
+					return {
+						value: e.id,
+						label: t(e.name) || e.name,
+					};
+				})}
+				searchable
+				creatable
+				getCreateLabel={(query) => t("Create ") + query}
+				onCreate={(query) => {
+					const item = { name: query };
+					dispatch(createCategory(item));
+					return item.name;
+				}}
+			/>
+
+			<Textarea
+				withAsterisk
+				label={t("About")}
+				placeholder={t("About the product") || "About the product"}
+				{...props.formObject.getInputProps("about")}
+			/>
+
+			<Group position="apart">
+				<Flex direction={"row"} gap="lg">
+					<NumberInput
+						withAsterisk
+						label={t("Price")}
+						step={250}
+						placeholder={t("Product price") || "Product price"}
+						{...props.formObject.getInputProps("price_current")}
+					/>
+					<NumberInput
+						withAsterisk
+						label={t("Quantity")}
+						placeholder={t("Quantity available") || "Quantity available"}
+						{...props.formObject.getInputProps("quantity_available")}
+					/>
+				</Flex>
+			</Group>
+		</>
 	);
 }
