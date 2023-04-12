b28d98677c7bc71434df1e41e12bba130824f136
Ahmed K. A <ahmadkabdullah@pm.me>
2023-04-12 19:24:25 +0300

    Remove user Name from users table

---
+++ pb_migrations/1681316533_updated_users.js
@@ -0,0 +1,29 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("users_name")
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "users_name",
+    "name": "name",
+    "type": "text",
+    "required": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
